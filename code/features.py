bb1b5c4a7c7e5e2e0d92ef0dcc6712ea42a60b3f
aa14k <>
2024-02-15 10:33:52 -0700

    added tile coding

--- code/features.py
+++ code/features.py
@@ -1,102 +1,62 @@
-"""
-Tile Coding Software version 3.0beta
-by Rich Sutton
-based on a program created by Steph Schaeffer and others
-External documentation and recommendations on the use of this code is available in the 
-reinforcement learning textbook by Sutton and Barto, and on the web.
-These need to be understood before this code is.
+import tiles3 as tc
+import numpy as np
 
-This software is for Python 3 or more.
 
-This is an implementation of grid-style tile codings, based originally on
-the UNH CMAC code (see http://www.ece.unh.edu/robots/cmac.htm), but by now highly changed. 
-Here we provide a function, "tiles", that maps floating and integer
-variables to a list of tiles, and a second function "tiles-wrap" that does the same while
-wrapping some floats to provided widths (the lower wrap value is always 0).
-
-The float variables will be gridded at unit intervals, so generalization
-will be by approximately 1 in each direction, and any scaling will have 
-to be done externally before calling tiles.
-
-Num-tilings should be a power of 2, e.g., 16. To make the offsetting work properly, it should
-also be greater than or equal to four times the number of floats.
-
-The first argument is either an index hash table of a given size (created by (make-iht size)), 
-an integer "size" (range of the indices from 0), or nil (for testing, indicating that the tile 
-coordinates are to be returned without being converted to indices).
-"""
-
-basehash = hash
-
-class IHT:
-    "Structure to handle collisions"
-    def __init__(self, sizeval):
-        self.size = sizeval                        
-        self.overfullCount = 0
-        self.dictionary = {}
-
-    def __str__(self):
-        "Prepares a string for printing whenever this object is printed"
-        return "Collision table:" + \
-               " size:" + str(self.size) + \
-               " overfullCount:" + str(self.overfullCount) + \
-               " dictionary:" + str(len(self.dictionary)) + " items"
-
-    def count (self):
-        return len(self.dictionary)
+class MountainCarTileCoder:
+    def __init__(self, iht_size=4096, num_tilings=8, num_tiles=8):
+        """
+        Initializes the MountainCar Tile Coder
+        Initializers:
+        iht_size -- int, the size of the index hash table, typically a power of 2
+        num_tilings -- int, the number of tilings
+        num_tiles -- int, the number of tiles. Here both the width and height of the
+                     tile coder are the same
+        Class Variables:
+        self.iht -- tc.IHT, the index hash table that the tile coder will use
+        self.num_tilings -- int, the number of tilings the tile coder will use
+        self.num_tiles -- int, the number of tiles the tile coder will use
+        """
+        self.iht = tc.IHT(iht_size)
+        self.num_tilings = num_tilings
+        self.num_tiles = num_tiles
     
-    def fullp (self):
-        return len(self.dictionary) >= self.size
-    
-    def getindex (self, obj, readonly=False):
-        d = self.dictionary
-        if obj in d: return d[obj]
-        elif readonly: return None
-        size = self.size
-        count = self.count()
-        if count >= size:
-            if self.overfullCount==0: print('IHT full, starting to allow collisions')
-            self.overfullCount += 1
-            return basehash(obj) % self.size
-        else:
-            d[obj] = count
-            return count
-
-def hashcoords(coordinates, m, readonly=False):
-    if type(m)==IHT: return m.getindex(tuple(coordinates), readonly)
-    if type(m)==int: return basehash(tuple(coordinates)) % m
-    if m==None: return coordinates
-
-from math import floor, log
-from itertools import zip_longest
-
-def tiles (ihtORsize, numtilings, floats, ints=[], readonly=False):
-    """returns num-tilings tile indices corresponding to the floats and ints"""
-    qfloats = [floor(f*numtilings) for f in floats]
-    Tiles = []
-    for tiling in range(numtilings):
-        tilingX2 = tiling*2
-        coords = [tiling]
-        b = tiling
-        for q in qfloats:
-            coords.append( (q + b) // numtilings )
-            b += tilingX2
-        coords.extend(ints)
-        Tiles.append(hashcoords(coords, ihtORsize, readonly))
-    return Tiles
-
-def tileswrap (ihtORsize, numtilings, floats, wrapwidths, ints=[], readonly=False):
-    """returns num-tilings tile indices corresponding to the floats and ints, wrapping some floats"""
-    qfloats = [floor(f*numtilings) for f in floats]
-    Tiles = []
-    for tiling in range(numtilings):
-        tilingX2 = tiling*2
-        coords = [tiling]
-        b = tiling
-        for q, width in zip_longest(qfloats, wrapwidths):
-            c = (q + b%numtilings) // numtilings
-            coords.append(c%width if width else c)
-            b += tilingX2
-        coords.extend(ints)
-        Tiles.append(hashcoords(coords, ihtORsize, readonly))
-    return Tiles
+    def get_tiles(self, position, velocity):
+        """
+        Takes in a position and velocity from the mountaincar environment
+        and returns a numpy array of active tiles.
+        
+        Arguments:
+        position -- float, the position of the agent between -1.2 and 0.5
+        velocity -- float, the velocity of the agent between -0.07 and 0.07
+        returns:
+        tiles - np.array, active tiles
+        """
+        # Set the max and min of position and velocity to scale the input
+        # POSITION_MIN
+        # POSITION_MAX
+        # VELOCITY_MIN
+        # VELOCITY_MAX
+        ### START CODE HERE ###
+        POSITION_MIN = -1.2
+        POSITION_MAX = 0.5
+        VELOCITY_MIN = -0.07
+        VELOCITY_MAX = 0.07
+        ### END CODE HERE ###
+        
+        # Use the ranges above and self.num_tiles to set position_scale and velocity_scale
+        # position_scale = number of tiles / position range
+        # velocity_scale = number of tiles / velocity range
+        
+        # Scale position and velocity by multiplying the inputs of each by their scale
+        
+        ### START CODE HERE ###
+        position_scale = self.num_tiles / (POSITION_MAX - POSITION_MIN)
+        velocity_scale = self.num_tiles / (VELOCITY_MAX - VELOCITY_MIN)
+        ### END CODE HERE ###
+        
+        # get the tiles using tc.tiles, with self.iht, self.num_tilings and [scaled position, scaled velocity]
+        # nothing to implment here
+        tiles = tc.tiles(self.iht, self.num_tilings, [position * position_scale, 
+                                                      velocity * velocity_scale])
+        
+        return np.array(tiles)
