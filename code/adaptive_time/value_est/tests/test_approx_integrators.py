fe3230ce6e288f742ee71c3d91ed02e26b8604c9
David Szepesvari <szepesvari.david@gmail.com>
2024-03-06 15:57:10 -0700

    Improved approximate integrators.

--- code/adaptive_time/value_est/tests/test_approx_integrators.py
+++ code/adaptive_time/value_est/tests/test_approx_integrators.py
@@ -7,45 +7,31 @@ from adaptive_time.value_est import approx_integrators
 
 
 class TestIntegrators(unittest.TestCase):
+    """Test the approximators for the integral of a reward sequence.
 
-    def test_approx_integral(self):
-        rewards = [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
-        idx = list(range(len(rewards)))
-        xs = np.array([rewards, idx])
-        tol = 0.001
-        idxes = {}
-        expected_integral = sum(rewards)
-        # Do the integration.
-        approx_integral = approx_integrators.approx_integrate(xs, tol, idxes)
-        num_pivots = len(idxes.keys())
-        print(f"approx_integral: {approx_integral} (true={expected_integral});"
-              f" num_pivots: {num_pivots}/{len(rewards)}")
-        self.assertAlmostEqual(approx_integral, expected_integral)
-        self.assertLess(num_pivots, len(rewards))
+    Most of these tests will use the same example data, just so it's easier to
+    think about. The reward sequence is:
+
+        dat: [0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
+        idx: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    The true integral is 26.
+    """
 
     def test_quad_integrator_precise(self):
         """Ensure we can run them, finding the true integral."""
-        rewards = [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
+        rewards = [0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
         true_integral = sum(rewards)
 
         quad = approx_integrators.AdaptiveQuadratureIntegrator(0.0)
         quad_integral, quad_pivots = quad.integrate(rewards)
         self.assertAlmostEqual(quad_integral, true_integral)
 
-    def test_unif_integrators_precise(self):
-        """Ensure we can run them, finding the true integral."""
-        rewards = [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
-        print(np.array([rewards, np.arange(len(rewards))]))
-        true_integral = sum(rewards)
-
-        uniform = approx_integrators.UniformlySpacedIntegrator(1)
-        uniform_integral, uniform_pivots = uniform.integrate(rewards)
-        self.assertAlmostEqual(uniform_integral, true_integral)
 
     def test_quadrature_tol(self):
         """Ensure we meet the tolerance requirement."""
-        rewards = [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
-        tol = 3.0
+        rewards = [0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
+        tol = 3.0   # A high tolerance so we don't have to use all data points.
         true_integral = sum(rewards)
 
         quad = approx_integrators.AdaptiveQuadratureIntegrator(tol)
@@ -53,46 +39,82 @@ class TestIntegrators(unittest.TestCase):
         self.assertLessEqual(abs(quad_integral - true_integral), tol)
         print(f"quad_pivots (num={len(quad_pivots)}/{len(rewards)}): {quad_pivots}")
 
-    def test_uniform2_odd(self):
-        """Make sure sampling every "second" point works decently.
-        
-        We actually see this is a little broken, we end up looking at each
-        data point.
-        """
-        rewards = [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
+    def test_unif_integrators_precise(self):
+        """Ensure u1 finds the true integral, using all points."""
+        rewards = [0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
         print(np.array([rewards, np.arange(len(rewards))]))
         true_integral = sum(rewards)
 
-        unif = approx_integrators.UniformlySpacedIntegrator(2)
-        unif_integral, unif_pivots = unif.integrate(rewards)
-        print("approx, true integral: ", unif_integral, true_integral)
-        print(f"unif_pivots (num={len(unif_pivots)}/{len(rewards)}): {unif_pivots}")
+        uniform = approx_integrators.UniformlySpacedIntegrator(1)
+        uniform_integral, uniform_pivots = uniform.integrate(rewards)
+        self.assertAlmostEqual(uniform_integral, true_integral)
+        self.assertEqual(len(uniform_pivots), len(rewards))
 
-    def test_uniform2_even(self):
-        """Make sure sampling every "second" point works decently.
+    def test_uniform2(self):
+        """Make sure sampling every second point towards a trapezoid approx works.
         
-        We actually see this is a little broken, we end up looking at each
-        data point.
+        Recall, the data is
+
+        ```
+            dat: [0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
+            idx: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        ```
+
+        This corresponds to doing estimation by forming trapezoids between
+        idxes 0 and 2, 2 and 4, 4 and 6, 6 and 8, 8 and 9 (since there is no 10).
+        This leads to trapezoid approximations of the partial sums of:
+        
+        ```
+            approx:    0+0+1=1, 1+2+3=6, 3+4+4=11, 4+4+4=12, 4+4=8,
+            through:     1.5       6       10.5      12       8         
+        ```
+        
+        But note that this double counts each intermediate point, so we just
+        subtract these out. Therefore we except the approximation to be
+        `1.5 + 6 + 10.5 + 12 + 8 - (1+3+4+4) = 38-12 = 26`.
         """
-        rewards = [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4]
+        rewards = [0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
         print(np.array([rewards, np.arange(len(rewards))]))
-        true_integral = sum(rewards)
+        expected_approx_integral = 26
+        expected_num_pivots = 6
 
         unif = approx_integrators.UniformlySpacedIntegrator(2)
         unif_integral, unif_pivots = unif.integrate(rewards)
-        print("approx, true integral: ", unif_integral, true_integral)
+        print("approx vs expected integral: ", unif_integral, expected_approx_integral)
         print(f"unif_pivots (num={len(unif_pivots)}/{len(rewards)}): {unif_pivots}")
+
+        self.assertEqual(unif_integral, expected_approx_integral)
+        self.assertEqual(len(unif_pivots), expected_num_pivots)
 
     def test_uniform5(self):
-        """Make sure sampling every "fifth" point works decently."""
-        rewards = [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
+        """Make sure sampling every fifth point works.
+        
+        See `test_uniform2` for a detailed explanation of the expected result.
+        Here I do a quick calculation to get the expected result.
+
+        ```
+        Orig data:
+            dat: [0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
+            idx: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        Workings:
+            1. 0->5: (0+4)/2 * 6 = 12
+            2. 5->9: (4+4)/2 * 5 = 20
+            total: 12+20 - 4 = 28
+        ```
+        """
+        rewards = [0, 0, 1, 2, 3, 4, 4, 4, 4, 4]
         print(np.array([rewards, np.arange(len(rewards))]))
-        true_integral = sum(rewards)
+        expected_approx = 28
+        expected_pivots = 3
 
         unif = approx_integrators.UniformlySpacedIntegrator(5)
         unif_integral, unif_pivots = unif.integrate(rewards)
-        print("approx, true integral: ", unif_integral, true_integral)
+        print("approx vs expected integral: ", unif_integral, expected_approx)
         print(f"unif_pivots (num={len(unif_pivots)}/{len(rewards)}): {unif_pivots}")
+
+        self.assertEqual(unif_integral, expected_approx)
+        self.assertEqual(len(unif_pivots), expected_pivots)
 
     # def test_on_real_data(self):
     #     """Use our own data."""
@@ -127,6 +149,20 @@ class TestIntegrators(unittest.TestCase):
     #             num_pivots[sampler_name].append(len(all_pivots))
     #     print(approx_integrals)
 
+    def test_approx_integral(self):
+        rewards = [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+        idx = list(range(len(rewards)))
+        xs = np.array([rewards, idx])
+        tol = 0.001
+        idxes = {}
+        expected_integral = sum(rewards)
+        # Do the integration.
+        approx_integral = approx_integrators.adaptive_approx_integrate(xs, tol, idxes)
+        num_pivots = len(idxes.keys())
+        print(f"approx_integral: {approx_integral} (true={expected_integral});"
+              f" num_pivots: {num_pivots}/{len(rewards)}")
+        self.assertAlmostEqual(approx_integral, expected_integral)
+        self.assertLess(num_pivots, len(rewards))
 
     @parameterized.expand([
         (1,), (2,), (3,), (4,), (5,)
